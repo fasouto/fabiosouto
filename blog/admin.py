1182c079c9cb233f4cd6a07ead0bd48786d498fb
Fabio Souto <fsoutomoure@gmail.com>
2010-10-28 15:50:23 +0200

    Added some filters to the admin

--- blog/admin.py
+++ blog/admin.py
@@ -10,11 +10,11 @@ admin.site.register(Category, CategoryAdmin)
 
 
 class EntryAdmin(admin.ModelAdmin):
-     list_display=['title','pub_date']
+     list_display=['title', 'pub_date', 'status']
      fieldsets = [
         (None,               {'fields': ['title','slug','body','body_markdown','tease','categories']}),
         ('Advanced options', {'fields': ['pub_date','enable_comments','featured','status'], 'classes': ['collapse']}),]
-     list_filter=['pub_date']
+     list_filter=['pub_date', 'status']
      search_fields = ['title']
      date_hierarchy = 'pub_date'
      prepopulated_fields = {"slug": ("title",)}
